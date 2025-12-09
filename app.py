e0dde305251557cfbb0ca540cbffec1ef623f177
Carloman <charles.em.brun@icloud.com>
2025-12-09 16:02:14 +0100

    Update app.py
    forgot to close a (

--- app.py
+++ app.py
@@ -664,7 +664,6 @@ Output format:
             )
         else:
             try:
-                assistant_reply = call_openrouter_raw(
                 raw_reply = call_openrouter_raw(
                     messages=or_messages,
                     model=main_model,
